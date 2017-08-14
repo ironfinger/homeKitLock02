4d69ad596533bc890bfe5f5d18b9d8b4062bbc29
ironfinger <tomhoughton888@gmail.com>
2017-08-14 19:42:11 +0100

    Initial Commit

---
+++ homeKitLock02/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  homeKitLock02
+//
+//  Created by Thomas Houghton on 14/08/2017.
+//  Copyright © 2017 Thomas Houghton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
